c0660aa276ee440fa7dc01ad6cdc38163f5a5490
free fire <mmfreefire123@gmail.com>
2025-08-24 15:51:41 +0000

    reduce the size more low and also why i cant see any space at the bottom

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -12,9 +12,9 @@ export function Hero() {
     offset: ['start start', 'end start'],
   });
 
-  const scale = useTransform(scrollYProgress, [0, 0.5], [1, 0.95]);
+  const scale = useTransform(scrollYProgress, [0, 0.5], [1, 0.98]);
   const borderRadius = useTransform(scrollYProgress, [0, 0.5], ['0px', '24px']);
-  const y = useTransform(scrollYProgress, [0, 0.5], ['0%', '2.5%']);
+  const y = useTransform(scrollYProgress, [0, 0.5], ['0%', '1%']);
 
   return (
     <section ref={targetRef} className="h-[200vh] relative">
